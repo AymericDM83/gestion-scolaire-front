f362c77025cabd1a2aa17fce046cf0f36194bbcc
sarra <sarra.mehamel@yahoo.com>
2023-02-23 11:36:27 +0100

    add  routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProfessorListComponent} from "./professor-components/professor-list/professor-list.component";
+import {AddProfessorComponent} from "./professor-components/add-professor/add-professor.component";
+import {ProfessorDetailsComponent} from "./professor-components/professor-details/professor-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path:'professors', component: ProfessorListComponent},
+  {path:'professors/add', component: AddProfessorComponent},
+  {path:'professors/:id', component: ProfessorDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
