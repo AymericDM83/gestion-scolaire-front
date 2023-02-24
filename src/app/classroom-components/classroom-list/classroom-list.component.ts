47a4c7e3bd1ee8c92c573ac4e7dcd29c8c2cd8a9
AymericDM <aymericmaggio@gmail.com>
2023-02-24 15:21:10 +0100

    back buttons 2/4

--- src/app/classroom-components/classroom-list/classroom-list.component.ts
+++ src/app/classroom-components/classroom-list/classroom-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Classroom } from '../../../model/classroom.model';
 import { ClassroomService } from '../../services/classroom.service';
 
@@ -9,8 +10,12 @@ import { ClassroomService } from '../../services/classroom.service';
 })
 export class ClassroomListComponent implements OnInit {
   classrooms: Classroom[] = [];
+  eId = this.activatedRoute.snapshot.paramMap.get('eId');
 
-  constructor(private classroomService: ClassroomService) {}
+  constructor(
+    private classroomService: ClassroomService,
+    private activatedRoute: ActivatedRoute
+  ) {}
   ngOnInit(): void {
     this.classroomService
       .findAll()
